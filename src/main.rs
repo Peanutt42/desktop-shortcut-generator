5b18975f62618509f487873bc6e53991c260e616
Peanutt42 <p3anutt42@gmail.com>
2024-02-24 18:03:08 +0100

    made shortcut file generation more robust

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,6 @@ use std::io::Write;
 use std::path::Path;
 use std::fs::File;
 
-struct Generator {
-	executable_path: String,
-	name: String,
-	icon_path: String,
-	comment: String,
-}
-
-impl Generator {
-	fn new() -> Self {
-		Self {
-			executable_path: String::new(),
-			name: String::new(),
-			icon_path: String::new(),
-			comment: String::new(),
-		}
-	}
-
-	fn generate(&self, filepath: &Path) -> Result<(), String> {
-		if !Path::new(&self.executable_path.clone()).exists() {
-			return Err(format!("{} doesn't exists", self.executable_path));
-		}
-		if !Path::new(&self.icon_path.clone()).exists() {
-			return Err(format!("{} doesn't exists", self.icon_path));
-		}
-		if self.name.is_empty() {
-			return Err("Name is empty".to_owned());
-		}
-
-		if let Ok(mut file) = File::create(filepath) {
-			if let Err(e) = file.write_all(format!(
-"[Desktop Entry]
-Version=1.0
-Type=Application
-Name={}
-Exec={}
-Icon={}
-Comment={}",
-				self.name,
-				self.executable_path,
-				self.icon_path,
-				self.comment
-			).as_bytes()) {
-				return Err(format!("Failed to write to {}: {e}", filepath.display()));
-			}
-			else {
-				return Ok(());
-			}
-		}
-
-		Err(format!("{} couldn't be created, missing priviledge? try running with 'sudo'", filepath.display()))
-	}
-}
-
 fn main() -> Result<(), eframe::Error> {
 	let options = eframe::NativeOptions {
 		viewport: egui::ViewportBuilder::default().with_inner_size([500.0, 200.0]),
@@ -72,7 +19,7 @@ fn main() -> Result<(), eframe::Error> {
 			
 			ui.horizontal(|ui| {
 				ui.label("Executable: ");
-				ui.text_edit_singleline(&mut generator.executable_path);
+				ui.label(&generator.executable_path);
 				if ui.button("Browse").clicked() {
 					if let Some(path) = rfd::FileDialog::new().pick_file() {
 						generator.executable_path = path.display().to_string();
@@ -87,10 +34,18 @@ fn main() -> Result<(), eframe::Error> {
 
 			ui.horizontal(|ui| {
 				ui.label("Icon: ");
-				ui.text_edit_singleline(&mut generator.icon_path);
+				ui.label(&generator.icon_path.clone().unwrap_or("Not specified".to_string()));
 				if ui.button("Browse").clicked() {
 					if let Some(path) = rfd::FileDialog::new().pick_file() { // todo: maybe add filter for png, jpg, ico and executables
-						generator.icon_path = path.display().to_string();
+						generator.icon_path = Some(path.display().to_string());
+					}
+					else {
+						generator.icon_path = None;
+					}
+				}
+				if generator.icon_path.is_some() {
+					if ui.button("Remove").clicked() {
+						generator.icon_path = None;
 					}
 				}
 			});
@@ -103,19 +58,23 @@ fn main() -> Result<(), eframe::Error> {
 			ui.separator();
 
 			ui.horizontal(|ui| {
-				if ui.add(egui::Button::new("Generate (current user only)").fill(egui::Color32::DARK_GREEN)).clicked() {
-					let home_path_str = home::home_dir();
-					if home_path_str.is_none() {
-						error_text = "Couldn't get linux home dir of the current user -> Are you running linux?".to_string();
-					}
-					else {
-						if let Err(e) = generator.generate(&Path::new(&home_path_str.unwrap()).join(format!(".local/share/applications/{}.desktop", generator.name))) {
-							error_text = e;
+				match dirs::home_dir() {
+					Some(home_dir) => {
+						if home_dir.display().to_string() != "/root" {
+							if ui.add(egui::Button::new("Generate (current user only)").fill(egui::Color32::DARK_GREEN)).clicked() {
+								if let Err(e) = generator.generate(&home_dir.join(&format!(".local/share/applications/{}.desktop", &generator.name))) {
+									error_text = e;
+								}
+							}
 						}
-					}					
+						else {
+							ui.colored_label(egui::Color32::RED, "Can't get the users home dir in sudo mode");
+						}
+					},
+					None => {},
 				}
 				if ui.add(egui::Button::new("Generate (global)").fill(egui::Color32::DARK_RED)).clicked() {
-					if let Err(e) = generator.generate(Path::new(&format!("/usr/share/applications/{}.desktop", generator.name))) {
+					if let Err(e) = generator.generate(Path::new(&format!("/usr/share/applications/{}.desktop", &generator.name))) {
 						error_text = e;
 					}
 				}
@@ -126,13 +85,70 @@ fn main() -> Result<(), eframe::Error> {
 		if !error_text.is_empty() {
 			egui::Window::new("Error")
 			.show(ctx, |ui| {
-				ui.visuals_mut().override_text_color = Some(epaint::Color32::RED);
-				ui.label(&error_text);
-				ui.visuals_mut().override_text_color = Some(epaint::Color32::WHITE);
+				ui.colored_label(egui::Color32::RED, &error_text);
 				if ui.button("Close").clicked() {
 					error_text.clear();
 				}
 			});
 		}
 	})
+}
+
+struct Generator {
+	executable_path: String,
+	name: String,
+	icon_path: Option<String>,
+	comment: String,
+}
+
+impl Generator {
+	fn new() -> Self {
+		Self {
+			executable_path: String::new(),
+			name: String::new(),
+			icon_path: None,
+			comment: String::new(),
+		}
+	}
+
+	fn generate(&self, filepath: &Path) -> Result<(), String> {
+		if !Path::new(&self.executable_path.clone()).exists() {
+			return Err(format!("Executable {} doesn't exists", self.executable_path));
+		}
+		if let Some(icon_path) = &self.icon_path {
+			if !Path::new(&icon_path.clone()).exists() {
+				return Err(format!("Icon {} doesn't exists", icon_path));
+			}
+		}
+		if self.name.is_empty() {
+			return Err("Name is empty".to_owned());
+		}
+
+		match File::create(filepath) {
+			Ok(mut file) => {
+				let mut file_content = "[Desktop Entry]\nVersion=1.0\nType=Application\n".to_string();
+				file_content.push_str(format!("Name={}\n", self.name).as_str());				
+				file_content.push_str(format!("Exec={}\n", self.executable_path).as_str());
+				if let Some(icon_path) = &self.icon_path {
+					file_content.push_str(format!("Icon={}\n", icon_path).as_str());
+				}
+				if !self.comment.is_empty() {
+					file_content.push_str(format!("Comment={}\n", self.comment).as_str());
+				}
+
+				if let Err(e) = file.write_all(file_content.as_bytes()) {
+					Err(format!("Failed to write to {}: {e}", filepath.display()))
+				}
+				else {
+					Ok(())
+				}
+			},
+			Err(e) => {
+				match e.kind() {
+					std::io::ErrorKind::PermissionDenied => Err(format!("{} couldn't be created due to missing privilidge -> try running with 'sudo'", filepath.display())),
+					_ => Err(format!("{} couldn't be created: {e}", filepath.display()))
+				}
+			},
+		}
+	}
 }
